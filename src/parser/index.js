628293ce22e001f5b4f04c190b33be7bdf334d95
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-22 15:36:45 -0300

    Parser initiaded

---
+++ src/parser/index.js
@@ -0,0 +1,13 @@
+import Tokenizer from "../tokenizer";
+
+export default class Parser extends Tokenizer{
+	constructor(input){
+		super(input);
+
+		this.input = input;
+	}
+
+	parse(){ //TODO };
+}
+
+
