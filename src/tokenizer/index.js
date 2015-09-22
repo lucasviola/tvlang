b2fe23d9b37c6448c32994379770e02aa9fec520
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-22 20:49:27 -0300

    Created functions that identifies tokens

--- src/tokenizer/index.js
+++ src/tokenizer/index.js
@@ -1,15 +1,25 @@
-import{ dictionary as dict } from "helpers/symbols";
-
+import { dictionary as dict } from "helpers/symbols";
+import { splitter as sp } from "helpers/splitter";
 
 //Tokenizer code
 // Turns Strings into key:value pairs (lexemmes)
 // E.g: var foo = 'bar';
 // <var: keyword> <foo:identifier> <bar:string>
 
+export function identifyTokens(token: String){
+	if(token in dict){
+		return value[token]
+	}	
+}
+
 export default class Tokenizer {
 	constructor(sentence){
 		this.sentence = sentence;
 	}
-
+	
 
 }
+
+
+
+
