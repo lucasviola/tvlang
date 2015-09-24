6a016476946ae16f6bae8d6c7504356ea18bd6da
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-24 20:01:56 -0300

    Starting on Lexeme

---
+++ src/tokenizer/lexeme.js
@@ -0,0 +1,15 @@
+// This should contain lexemes, which are
+// <key:value> pairs
+
+export default class Lexeme {
+	constructor(key, value){
+		this.key = key;
+		this.value = value;
+	};
+
+	var symbols = {}
+
+
+
+}
+
