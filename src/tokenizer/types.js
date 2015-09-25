dca6f1d970beca1f97ff98cf437e00ce2fbeed66
lucasviola <lucasmatzenbacher@gmail.com>
2015-09-25 19:40:21 -0300

    Added TokenTypes definition class

---
+++ src/tokenizer/types.js
@@ -0,0 +1,9 @@
+// Token Types are defined here
+// Based on Babylon (Babel's parser): https://github.com/babel/babel
+
+export class TokenType {
+	constructor(name, conf = {}){
+		this.name = name;	
+	}
+
+}
